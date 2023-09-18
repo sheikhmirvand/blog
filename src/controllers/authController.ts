f196711955f6ab974fd8e3dcc59c2877eb47730a
sheikhmirvand <92518678+sheikhmirvand@users.noreply.github.com>
2023-09-18 21:03:02 +0330

    add an await to project (:

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -68,7 +68,7 @@ class AuthController {
                 return res.status(400).json({ message: "ابتدا ثبت نام کنید" });
 
             // check valid password
-            const isCompare = bcryt.compare(password, user.password as string);
+            const isCompare = await bcryt.compare(password, user.password as string);
 
             if (!isCompare)
                 return res.status(400).json({ message: "پسور درست نیست" });
